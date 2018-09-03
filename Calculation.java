3b7b644b6264a27ab5b4ac4d204e2319559df854
Saranya Selvarasu <SSelvarasu@corp.encoress.com>
2018-09-03 16:10:36 +0530

    java code added

---
+++ Calculation.java
@@ -0,0 +1,14 @@
+class Calculation
+{
+   void sub(int x,int y)
+      {
+            int z;
+	          z=x-y;
+		        System.out.println("Sub="+z);
+			   }
+			      public static void main(String args[])
+			         {
+				       Calculation c=new Calculation();
+				             c.sub(200,50);
+					        }
+						}
