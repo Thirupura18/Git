a0371513d3aaa3227235d690e27f6a2aa6288147
Saranya Selvarasu <SSelvarasu@corp.encoress.com>
2018-09-03 17:11:54 +0530

    addition is added with it

--- Calculation.java
+++ Calculation.java
@@ -1,17 +1,33 @@
 
-// Subtraction Program
+// Subtraction and Addition Program
 public class Calculation
 {
    void sub(int x,int y)
       {
             int z;
-	    z=x-y;
-		        
-           System.out.println("Sub="+z);
+	    z=x-y;	        
+            System.out.println("Sub="+z);
        }
-       public static void main(String args[])
-       {
+
+
+   void add(int x,int y)
+      {
+             int z;
+             z=x+y;
+             System.out.println("add"+z);
+      }
+
+      public static void main(String args[])
+       {           
             Calculation c=new Calculation();
             c.sub(200,50);
+	    c.add(10,30);
        }
 }
+
+
+
+
+
+
+   
