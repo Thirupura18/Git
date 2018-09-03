cd3f7f929a94ceb10f7344e16a1e168193e89665
Saranya Selvarasu <SSelvarasu@corp.encoress.com>
2018-09-03 16:27:17 +0530

    code is altered

--- Calculation.java
+++ Calculation.java
@@ -1,14 +1,17 @@
-class Calculation
+
+// Subtraction Program
+public class Calculation
 {
    void sub(int x,int y)
       {
             int z;
-	          z=x-y;
-		        System.out.println("Sub="+z);
-			   }
-			      public static void main(String args[])
-			         {
-				       Calculation c=new Calculation();
-				             c.sub(200,50);
-					        }
-						}
+	    z=x-y;
+		        
+           System.out.println("Sub="+z);
+       }
+       public static void main(String args[])
+       {
+            Calculation c=new Calculation();
+            c.sub(200,50);
+       }
+}
