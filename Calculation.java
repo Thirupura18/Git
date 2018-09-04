ac182c0c6f6fc9d26f3d803dbb2f1478904ffd95
Thirupura Sundari Jayakumar <TJayakumar@corp.encoress.com>
2018-09-04 14:59:26 +0530

    fork

--- Calculation.java
+++ Calculation.java
@@ -17,14 +17,36 @@ public class Calculation
              System.out.println("add"+z);
       }
 
+
+
+      void mul(int x,int y)
+      {
+	int z;
+	z=x*y;
+	System.out.println("mul :"+z);
+
+      
+     }
+      
+      void div(int x,int y)
+      {
+
+      int z;
+      z=x/y;
+      System.out.println("div "+z);
+      }
+     
+
       public static void main(String args[])
        {           
             Calculation c=new Calculation();
             c.sub(200,50);
 	    c.add(10,30);
+	    c.mul(5,6);
+	    c.div(10,2);
        }
 }
-
+//end of the program
 
 
 
